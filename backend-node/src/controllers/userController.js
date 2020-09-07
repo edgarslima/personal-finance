800007c0c620b738811e3d08c170388f625748e6
Edgar Lima <edgar.lima@outlook.com>
2020-09-07 18:40:16 -0300

    update

--- backend-node/src/controllers/userController.js
+++ backend-node/src/controllers/userController.js
@@ -18,10 +18,10 @@ module.exports = {
     },
 
     async create (request, response) {
-        const {usr_key, usr_passord, usr_grpusr_id, usr_status} = request.body;
-        var strQuery = 'INSERT INTO users (usr_key, usr_passord, usr_grpusr_id, usr_status, usr_creation) ';
+        const {usr_key, usr_password, usr_grpusr_id, usr_status} = request.body;
+        var strQuery = 'INSERT INTO users (usr_key, usr_password, usr_grpusr_id, usr_status, usr_creation) ';
         strQuery += 'VALUES (?, ?, ?, ?, now()) ';
-        await pool.query(strQuery, [usr_key, usr_passord, usr_grpusr_id, usr_status]);
+        await pool.query(strQuery, [usr_key, usr_password, usr_grpusr_id, usr_status]);
         response.status = 200;
         return response.json({"status": "OK"})
     },
@@ -36,10 +36,10 @@ module.exports = {
 
     async update (request, response) {
         const {usr_key} = request.params;
-        const {usr_passord, usr_grpusr_id, usr_status} = request.body;
-        var strQuery = 'UPDATE users SET usr_passord = ?, usr_grpusr_id = ?, usr_status = ?  ';
+        const {usr_password, usr_grpusr_id, usr_status} = request.body;
+        var strQuery = 'UPDATE users SET usr_password = ?, usr_grpusr_id = ?, usr_status = ?  ';
         strQuery += ' WHERE usr_key = ? ';
-        await pool.query(strQuery, [usr_passord, usr_grpusr_id, usr_status, usr_key]);
+        await pool.query(strQuery, [usr_password, usr_grpusr_id, usr_status, usr_key]);
         response.status = 200;
         return response.json({"status": "OK"})
     }
